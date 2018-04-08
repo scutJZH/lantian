b6648fa1cddde7c70146593e417a3408590e345b
jzh <745878318@qq.com>
2018-04-08 11:43:05 +0800

    作业、添加孩子、删除孩子、注销模块

--- src/main/java/org/scut/service/publicService/ILogService.java
+++ src/main/java/org/scut/service/publicService/ILogService.java
@@ -2,6 +2,10 @@ package org.scut.service.publicService;
 
 import java.util.Map;
 
-public interface ILoginService {
+public interface ILogService {
+	
 	public Map<String, Object> login(String userType, String telnumber, String pwd, String token);
+	
+	public Map<String, String> logout(String userType, String id, String token);
+	
 }
