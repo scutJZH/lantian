0dffcc630c9419bfcf7a286f333319a07d9b8e39
tanshengjun <1002188561@qq.com>
2018-04-16 18:03:16 +0800

    .

--- src/main/java/org/scut/controller/TeacherCourseModule.java
+++ src/main/java/org/scut/controller/TeacherCourseModule.java
@@ -15,11 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value="/TeacherCourseModule")
 public class TeacherCourseModule {
-@RequestMapping(value="/TeacherCourseModule_test")
+@RequestMapping(value="/getHomeworkList")
 @ResponseBody
-public Map<String,Object> test(@RequestParam(value="teacher_id",required=false)String teacher_id,@RequestParam(value="class_id",required=false)String class_id) {
+public Map<String,Object> getHomeworkList(@RequestParam(value="teacher_id",required=false)String teacher_id,
+		@RequestParam(value="class_id",required=false)String class_id) {
 	String teacherid=teacher_id;
 	String classid=class_id;
+	
 	Map<String,Object> m=new HashMap<String,Object>();
 	m.put("status", "123232");
 	m.put("data", "213123");
