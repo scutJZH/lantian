22f0c4a5e2d33133d124b3a9360f727cf586e0c4
jzh <745878318@qq.com>
2018-04-17 19:04:06 +0800

    测试好注册并修改完注册产生的bug

--- src/main/java/org/scut/model/Student.java
+++ src/main/java/org/scut/model/Student.java
@@ -21,6 +21,7 @@ public class Student extends User{
 		this.schoolName = schoolName;
 	}
 	
+	
 
 
     
--- src/main/java/org/scut/model/Teacher.java
+++ src/main/java/org/scut/model/Teacher.java
@@ -24,6 +24,7 @@ public class Teacher extends User{
 		this.schoolName = schoolName;
 	}
 	
+	
 
 	
 	
